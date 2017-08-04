22d6f7cffa54c6a44fa14fd7c6035baa37e27fd0
Josafat Daniel Salcedo Sambrano <dsalcedo@csgroup.mx>
2017-08-04 10:04:18 -0500

    y el nombre de la tabla ??

--- app/Tags.php
+++ app/Tags.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tags extends Model
 {
+    // Y el nombre de la tabla ????  protected $table = 'my_table_name_was_here';
     public $fillable = ['name','details'];
 }
 
